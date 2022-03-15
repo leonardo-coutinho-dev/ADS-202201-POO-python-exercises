bbf1372ae656b61c03001e5d324d1d4c5f63a513
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-03-15 07:01:23 -0300

    First commit

---
+++ exercicios-live-1.py
@@ -0,0 +1,15 @@
+def nome_da_funcao():
+    print('Olá mundo')
+
+
+def soma_dois_numeros(x, y):
+    z = x + y
+    print(z)
+
+
+soma = 0
+for i in range(1, 50, 2):
+    soma += i
+print(soma)
+
+soma_dois_numeros(2, 10)
